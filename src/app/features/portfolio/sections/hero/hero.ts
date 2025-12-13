7c69b1316ff76684c7d9b3f222a4df7cf6c0bb28
Mauricio Vereau <mauricio.vereau@gmail.com>
2025-12-13 06:49:33 -0500

    Descarga de CV por idioma

--- src/app/features/portfolio/sections/hero/hero.ts
+++ src/app/features/portfolio/sections/hero/hero.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BadgeHello } from "../../components/badge-hello/badge-hello";
 import { TypedText } from "../../components/typed-text/typed-text";
 import { TranslatePipe } from '../../../../core/pipe/TranslatePipe.pipe';
+import { TranslationService, LangCode } from '../../../../core/services/translation.service';
 
 @Component({
   selector: 'app-hero',
@@ -9,5 +10,20 @@ import { TranslatePipe } from '../../../../core/pipe/TranslatePipe.pipe';
   templateUrl: './hero.html',
 })
 export class Hero {
+  translate = inject(TranslationService);
+
+  downloadCV(): void{
+    const lang = this.translate.lang
+
+    const cvPath = lang === 'en'? 'assets/cv/MauricioVereau-EN.pdf':'assets/cv/MauricioVereau-ES.pdf';
+
+    const link = document.createElement('a');
+    link.href = cvPath;
+    link.download = lang === 'en' ? 'MauricioVereau-EN.pdf': 'MauricioVereau-ES.pdf';
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
 }
