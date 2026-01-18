c75f8f6cbe06196956c8dca188375afc98851c2e
Mauricio Vereau <mauricio.vereau@gmail.com>
2026-01-17 21:09:04 -0500

    correciones de errores

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
@@ -16,11 +15,5 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideHttpClient(withFetch()),
-    importProvidersFrom(
-      ServiceWorkerModule.register('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:3000'
-      })
-    )
   ]
 };
